dae38f15d44c0dda1f679fff4b3370df4fc42019
paruljamwal <paruljamwal2000@gmail.com>
2022-12-16 00:37:35 +0530

    life style store backend

--- server.js
+++ server.js
@@ -3,10 +3,10 @@ const app = require("./index");
 //importing connect
 const connect = require("./configs/db");
 
-app.listen(5000, async function () {
+app.listen(8080, async function () {
 	try {
 		await connect();
-		console.log("Listening to Port 5000");
+		console.log("Listening to Port 8080");
 	} catch (e) {
 		console.error({ error: e.message });
 	}
